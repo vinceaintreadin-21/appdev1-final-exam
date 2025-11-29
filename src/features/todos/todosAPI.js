4688f73934750f334784ee01a9b9db6f12d69831
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-29 08:58:50 +0800

    Implement API helper using axios

---
+++ src/features/todos/todosAPI.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+export const getTodosAPI = async () => {
+    try {
+        const res = await axios.get(`${import.meta.env.VITE_APP_API_URL}/todos?_limit=10`)
+        console.log(res.data)
+        return res.data
+    } catch (err) {
+        console.error('Error fetching todos:', err)
+         throw err
+    }
+}
+
+export const addTodoAPI = async (todo) => {
+    try {
+        const res = await axios.post(`${import.meta.env.VITE_APP_API_URL}/todos`, todo, {
+            headers: { 'Content-Type': 'application/json'}
+        })
+
+        console.log('Added Todo task in the List', res.data)
+        return res.data
+    } catch (err) {
+        console.error('Error on adding todo task into the list', err)
+        throw err
+    }
+}
+
+export const updateTodoAPI = async (todo) => {
+    try {
+        const res = await axios.put(`${import.meta.env.VITE_APP_API_URL}/todos/${todo.id}`, todo, {
+            headers: { 'Content-Type': 'application/json'}
+        })
+
+        console.log('Updated Todo task', res.data)
+        return res.data
+    } catch (err) {
+        console.error('Error on updating todo task into the list', err)
+        throw err
+    }
+}
+
+export const deleteTodoAPI = async (id) => {
+    try {
+        const res = await axios.delete(`${import.meta.env.VITE_APP_API_URL}/${id}`)
+        console.log('deleted task successfully', res)
+        return res.data
+    } catch (err) {
+        console.error('Error on deleting todo task into the list', err)
+        throw err
+    }
+}
